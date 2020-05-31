70ac675c5ba9ddb59b1c6f39c803946ed0b9b045
Sourav Menon <45852906+souravmenon@users.noreply.github.com>
2020-05-31 23:42:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,41 @@ class LinearRegression(object):
   """
   An implementation of linear regression model
   """
-  from sklearn.linear_model import LinearRegression
+ 
+ 
+ 
   
-  clf=LinearRegression()
   def fit(_input, _output):
-    pass
+      x=_input
+      y=_output
+      def mean(values):
+	        return sum(values) / float(len(values))
+ 
+      # Calculate covariance between x and y
+      def covariance(x, mean_x, y, mean_y):
+	        covar = 0.0
+	        for i in range(len(x)):
+		          covar += (x[i] - mean_x) * (y[i] - mean_y)
+	        return covar
+ 
+      # Calculate the variance of a list of numbers
+      def variance(values, mean):
+	        return sum([(x-mean)**2 for x in values])
+ 
+       # Calculate coefficients
+
+	   x_mean, y_mean = mean(x), mean(y)
+	   b1 = covariance(x, x_mean, y, y_mean) / variance(x, x_mean)
+	   b0 = y_mean - b1 * x_mean
+	   return [b0, b1]
+    
+  b0,b1=fit(_input,_output)
     
   def predict(_input):
-    pass
+    for row in test:
+		    yhat = b0 + b1 * _input
+		    predictions.append(yhat)
+	   return predictions
+    
     
     
